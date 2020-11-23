244eecb2cda11f3cd4c1150f55b72fd8718d101e
Opilane TTHK <opilane@tthk.ee>
2020-11-23 13:51:46 +0200

    Dog

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const currentImage = document.getElementById("current");
+console.log(currentImage);
+const galleryImages = document.querySelectorAll(".img");
+console.log(galleryImages);
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", (event) => {
+        galleryImages.forEach(img => {
+            img.style.opacity = 1;
+        });
+        currentImage.src = event.target.src;
+        event.target.style.opacity = 0.5;
+    });
+});
